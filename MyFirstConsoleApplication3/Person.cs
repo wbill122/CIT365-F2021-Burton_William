f46fed0ba047a25cb04ba1ed75759059be42e2e2
billburton405@gmail.com <billburton405@gmail.com>
2021-09-15 22:09:56 -0700

    First push

---
+++ MyFirstConsoleApplication3/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication3
+{
+    class Person
+    {
+        public string nam;
+        public string location;
+    }
+}
---
+++ MyFirstConsoleApplication3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication3
+{
+    class Program
+    {
+        static void GetUserNameAndLocation()
+
+        {
+            Person person = new Person();
+
+            Console.Write("What is your name?");
+            person.name = Console.ReadLine();
+            
+            Console.Write($"Hi {person.name}!  Where are you from? ");
+            person.location = Console.ReadLine();
+            
+            Console.WriteLine($"I have never been to {person.location}.  I bet it is nice. ");
+            Console.Write("Press any key to continue");
+            
+            Console.ReadKey();
+        }
+
+        static void ChristmasCountdown(DateTime myDateTime)
+        { Console.WriteLine($"Today's date is: {myDateTime:d}");
+            int daysUntilChristmas = ((TimeSpan)(new DateTime(myDateTime.Year, 12, 25) - myDateTime)).Days;
+          
+            Console.WriteLine($"There are {daysUntilChristmas} day until Christmas!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+        static void Main()
+        {
+            GetUserNameAndLocation();
+            ChristmasCountdown(DateTime.Now);
+          
+
+            Console.WriteLine("Press any key to end program");
+            Console.ReadKey();
+        }
+    }
+}
+
+
