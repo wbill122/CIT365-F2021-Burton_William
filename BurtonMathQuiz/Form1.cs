927fbbb890c31ac8dab4c8718908f1a05b5107a8
billburton405@gmail.com <billburton405@gmail.com>
2021-09-20 21:32:35 -0700

    9_20_21

--- BurtonMathQuiz/Form1.cs
+++ BurtonMathQuiz/Form1.cs
@@ -26,6 +26,7 @@ namespace BurtonMathQuiz
         int timeLeft;
 
         public void StartTheQuiz()
+        
         {
             // Fill in the addition problem.
             // Generate two random numbers to add.
@@ -49,6 +50,19 @@ namespace BurtonMathQuiz
             timeLabel.Text = "30 seconds";
             timer1.Start();
         }
+        /// <summary>
+        /// Check the answer to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
+        }
+
+
 
         public Form1()
         {
