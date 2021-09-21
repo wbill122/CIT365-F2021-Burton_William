e7a445776258fa647b1f25d56ccc74a3228115d3
billburton405@gmail.com <billburton405@gmail.com>
2021-09-20 21:39:08 -0700

    9_20_21

--- BurtonMathQuiz/Form1.cs
+++ BurtonMathQuiz/Form1.cs
@@ -46,8 +46,8 @@ namespace BurtonMathQuiz
             sum.Value = 0;
 
             // Start the timer.
-            timeLeft = 30;
-            timeLabel.Text = "30 seconds";
+            timeLeft = 15;
+            timeLabel.Text = "15 seconds";
             timer1.Start();
         }
         /// <summary>
@@ -92,16 +92,28 @@ namespace BurtonMathQuiz
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (timeLeft > 0)
+            if (CheckTheAnswer())
             {
-                // Display the new time left
-                // by updating the Time Left label.
-                timeLeft = timeLeft - 1;
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                    "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() return false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft--;
                 timeLabel.Text = timeLeft + " seconds";
             }
             else
             {
-                // If the user ran out of time, stop the timer, show
+                // If the user ran out of time, stop the timer, show 
                 // a MessageBox, and fill in the answers.
                 timer1.Stop();
                 timeLabel.Text = "Time's up!";
