c7b3c5aad107ef0da646b668b1da9a5b74e2c73f
billburton405@gmail.com <billburton405@gmail.com>
2021-10-03 13:42:55 -0700

    Test

--- MegaDesk-Burton01/AddQuote.Designer.cs
+++ MegaDesk-Burton01/AddQuote.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace MegaDesk_Burton01
 {
-    partial class Form1
+    partial class AddQuote
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace MegaDesk_Burton01
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "AddQuote";
         }
 
         #endregion
     }
 }
-
--- MegaDesk-Burton01/SearchQuotes.cs
+++ MegaDesk-Burton01/SearchQuotes.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MegaDesk_Burton01
 {
-    public partial class Form1 : Form
+    public partial class SearchQuotes : Form
     {
-        public Form1()
+        public SearchQuotes()
         {
             InitializeComponent();
         }
